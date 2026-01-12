14e70afc6f3187b54d412cbdede177d73c9d23f0
SolarCyclone <190097766+SolarCyclone@users.noreply.github.com>
2026-01-12 18:03:50 +0800

    Create aboutus.jsx

---
+++ components/aboutus.jsx
@@ -0,0 +1,73 @@
+// needs change in content
+
+import React from 'react';
+import './aboutus.css';
+import picture from '../images/picture_aboutus.png';
+
+function AboutUs() {
+    return (
+        <main className="aboutUsMain">
+            <div className='about-us-header'>
+                <h1>About Us</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos iure repudiandae quasi ea aut nesciunt consectetur maiores labore. Inventore atque, voluptatum voluptates sunt nihil asperiores magnam non saepe ad ut!</p>
+            </div>
+            <div>
+                <div className='meet-the-team'>
+                    <h1>Meet the Team</h1>
+                </div>
+                <div className="containerAboutUs">
+                    <img className="picture" src={picture} width={"350px"} height={"300px"}></img>
+                    <div className='team-name'>
+                        <h2>Name Here</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum ad consequatur et a officiis veniam saepe nostrum iusto aperiam? Fugiat, odit tenetur sapiente quidem similique dicta perferendis? Ut, eligendi esse.</p>
+                    </div>
+                    <div className='team-socials'>
+                        <p>optional link to socials/resume/linkedin here</p>
+                    </div>
+                </div>
+                <div className="containerAboutUs">
+                    <img className="picture" src={picture} width={"350px"} height={"300px"}></img>
+                    <div className='team-name'>
+                        <h2>Name Here</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum ad consequatur et a officiis veniam saepe nostrum iusto aperiam? Fugiat, odit tenetur sapiente quidem similique dicta perferendis? Ut, eligendi esse.</p>
+                    </div>
+                    <div className='team-socials'>
+                        <p>optional link to socials/resume/linkedin here</p>
+                    </div>
+                </div>
+                <div className="containerAboutUs">
+                    <img className="picture" src={picture} width={"350px"} height={"300px"}></img>
+                    <div className='team-name'>
+                        <h2>Name Here</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum ad consequatur et a officiis veniam saepe nostrum iusto aperiam? Fugiat, odit tenetur sapiente quidem similique dicta perferendis? Ut, eligendi esse.</p>
+                    </div>
+                    <div className='team-socials'>
+                        <p>optional link to socials/resume/linkedin here</p>
+                    </div>
+                </div>
+                <div className="containerAboutUs">
+                    <img className="picture" src={picture} width={"350px"} height={"300px"}></img>
+                    <div className='team-name'>
+                        <h2>Name Here</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum ad consequatur et a officiis veniam saepe nostrum iusto aperiam? Fugiat, odit tenetur sapiente quidem similique dicta perferendis? Ut, eligendi esse.</p>
+                    </div>
+                    <div className='team-socials'>
+                        <p>optional link to socials/resume/linkedin here</p>
+                    </div>
+                </div>
+                <div className="containerAboutUs">
+                    <img className="picture" src={picture} width={"350px"} height={"300px"}></img>
+                    <div className='team-name'>
+                        <h2>Name Here</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum ad consequatur et a officiis veniam saepe nostrum iusto aperiam? Fugiat, odit tenetur sapiente quidem similique dicta perferendis? Ut, eligendi esse.</p>
+                    </div>
+                    <div className='team-socials'>
+                        <p>optional link to socials/resume/linkedin here</p>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+};
+
+export default AboutUs
