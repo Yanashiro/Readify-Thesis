d845c9d6dd175afcefa3226674ab86b0838aacae
SolarCyclone <190097766+SolarCyclone@users.noreply.github.com>
2026-01-15 11:57:12 +0800

    Update root.jsx

--- components/root.jsx
+++ components/root.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Navbar from './navbar'
-import Login from './login'
-import Signup from './signup'
+import Navbar from './main-components/navbar'
+import Login from './main-components/login'
+import Signup from './main-components/signup'
 import './maintestboard.css'
 
 const container = document.getElementById('root');
