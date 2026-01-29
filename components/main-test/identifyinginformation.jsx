9d9c8dc02bada800337b32a163c02609e3f12dc1
SolarCyclone <190097766+SolarCyclone@users.noreply.github.com>
2026-01-29 21:04:39 +0800

    Update identifyinginformation.jsx

--- components/main-test/identifyinginformation.jsx
+++ components/main-test/identifyinginformation.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import SideTimer from '../main-components/timer';
+import SideTimer from './timer';
+import { useCookies } from 'react-cookie';
 import './maintestpage.css';
 
 function IdentifyingInformation() {
 
-    const [userAnswers, setUserAnswers] = useState({});
-    const [currentPage, setCurrentPage] = useState(0);
-    const [allQuestions, setAllQuestions] = useState([]);
-    const [passageHistory, setPassageHistory] = useState([]);
+    const [cookies] = useCookies(['examinee-cookie'])
+    const [userAnswers, setUserAnswers] = useState(() => {
+        const saved = sessionStorage.getItem("Answer");
+        return saved ? JSON.parse(saved) : {}});
+    const [currentPage, setCurrentPage] = useState(() => {
+        const saved = sessionStorage.getItem("Page History");
+        return saved ? JSON.parse(saved) : 0});
+    const [allQuestions, setAllQuestions] = useState(() => {
+        const saved = sessionStorage.getItem("Questions History");
+        return saved ? JSON.parse(saved) : []});
+    const [passageHistory, setPassageHistory] = useState(() => {
+        const saved = sessionStorage.getItem("Passage History");
+        return saved ? JSON.parse(saved) : []});
+    const [fontSize, setFontSize] = useState(() => {
+        const saved = sessionStorage.getItem("Font Size");
+        return saved ? JSON.parse(saved) : 20});
+    const [time, setTime] = useState(() => {
+        const saved = sessionStorage.getItem("Timer remain");
+        return saved ? JSON.parse(saved) : 900});
 
     // stores passage history when clicking the "back" button array of currentPage serves as an updator of the page, see line 84
     const currentPassage = passageHistory[currentPage];
@@ -21,11 +37,14 @@ function IdentifyingInformation() {
     const indexOfFirstQuestion = indexOfLastQuestion - questionsPerPage;
     // used to print out current questions from "allQuestions" hook (remember it was intercepted by setAllQuestions at Axios call line 31) Excluding those that have been "sliced"
     const currentQuestions = allQuestions.slice(indexOfFirstQuestion, indexOfLastQuestion);
+    const questionNumberStart = indexOfFirstQuestion;
+    const questionNumberEnd = indexOfLastQuestion;
 
     // initial request of data from the backend
     useEffect(() => {
+        if (passageHistory.length === 0) {
         axios
-            .post('/maintestroute/hello')
+            .post('/maintestroute/identifyinginformation')
             .then((res) => {
                 console.log("Number of question received", res.data.questions.length);
                 console.log("Questions Array:", res.data.questions);
@@ -35,7 +54,18 @@ function IdentifyingInformation() {
                 setPassageHistory([res.data]);
             })
             .catch((err) => console.error(err));
+        }
     }, [])
+
+    // immediat3e sessionStorage collecting
+    useEffect(() => {
+        sessionStorage.setItem("Answer", JSON.stringify(userAnswers));
+        sessionStorage.setItem("Font Size", fontSize);
+        sessionStorage.setItem("Passage History", JSON.stringify(passageHistory));
+        sessionStorage.setItem("Page History", JSON.stringify(currentPage));
+        sessionStorage.setItem("Questions History", JSON.stringify(allQuestions));
+        sessionStorage.setItem("Timer remain", time)
+    }, [userAnswers, fontSize, currentPage, allQuestions, passageHistory, time]);
 
     const userChoiceClick = (questionId, choiceValue) => {
         // used to save user choices (answers) even if the page is moved
@@ -43,6 +73,25 @@ function IdentifyingInformation() {
             ...prev,
             [questionId]: choiceValue
         }))
+    }
+
+    // increase font size of passage
+    const increaseFontSize = () => {
+        if (fontSize == 40) {
+            return;
+        }
+        setFontSize(prevSize => prevSize + 2);
+    }
+    // return to default font size in passage
+    const defaultFontSize = () => {
+        setFontSize(20)
+    }
+    // decrease font size of passage
+    const decreaseFontSize = () => {
+        if (fontSize == 10) {
+            return;
+        }
+        setFontSize(prevSize => prevSize - 2);
     }
 
     const handleNextPage = () => {
@@ -60,7 +109,7 @@ function IdentifyingInformation() {
             // if length of allQuestions array is greater than variable totalLimit, function returns nothing
             return; // stops the function
         }
-            axios.post('/maintestroute/hello')
+            axios.post('/maintestroute/identifyinginformation')
                 .then((res) => {
                     setAllQuestions(prevQuestions => {
                         const combined = [...prevQuestions, ...res.data.questions]
@@ -75,6 +124,32 @@ function IdentifyingInformation() {
 
     if (allQuestions.length === 0) return <h1>Loading...</h1>
 
+    const sendUserAnswers = () => {
+        
+        const submissionData = {
+            examinee: cookies['examinee-cookie'],
+            answers: userAnswers,
+            data: new Date()
+        };
+        
+        axios
+            .post('/maintestroute/identifyinginformation', submissionData)
+            .then((res) => {
+                if (res.status == 200) {
+                    window.location.replace('/maintest/examsubmitted');
+                    sessionStorage.removeItem("Answer")
+                    sessionStorage.removeItem("Font Size")
+                    sessionStorage.removeItem("Passage History")
+                    sessionStorage.removeItem("Page History")
+                    sessionStorage.removeItem("Questions History")
+                    sessionStorage.removeItem("Timer remain")
+                }
+            })
+            .catch((err) => {
+                alert("Submission failed. Please check your internet and try again.")
+                console.error(err)});
+    }
+
     return (
         <>
         <main className='main-maintest'>
@@ -84,7 +159,7 @@ function IdentifyingInformation() {
                     <h1 className='name'>Readify</h1>
                 </div>
                 <div className='timer-component'>
-                    <h3 className='sidetimer-h2'><SideTimer /></h3>
+                    <h3 className='sidetimer-h2'><SideTimer time={time} setTime={setTime}/></h3>
                 </div>
                 <div className='warning-tab'>
                     <p className='warning-text'>Warning! Multiple<br /> tab changes can result in exam <br />termination</p>
@@ -95,6 +170,17 @@ function IdentifyingInformation() {
                     <div className='title-div'>
                         {/* This is the exam category*/}
                         <h1 className='h1-title-div'>{currentPassage?.testTitle}</h1>
+                    </div>
+                    <div className='view-size-buttons'>
+                        <button className='font-size-btn' onClick={decreaseFontSize}>
+                            Decr
+                        </button>
+                        <button className='font-size-btn' onClick={defaultFontSize}>
+                            Default
+                        </button>
+                        <button className='font-size-btn' onClick={increaseFontSize}>
+                            Incr
+                        </button>
                     </div>
                 </section>
                 <div className='two-sections'>
@@ -109,13 +195,13 @@ function IdentifyingInformation() {
                         </div>
                         {/* This is the passage text/content */}
                         <div className='test-passage'>
-                            <>{currentPassage?.passage}</>
+                            <p style={{fontSize: `${fontSize}px`}}>{currentPassage?.passage}</p>
                         </div>
                     </div>
                     <section className='questions-side'>
                         <div>
                             <div>
-                                <b><p className='p-questionRange'>{currentPassage?.questionNoRange}</p></b>
+                                <b><p className='p-questionRange'>Questions {questionNumberStart + 1}{questionNumberEnd <= 10 ? `-${questionNumberEnd}` : ''}</p></b>
                                 <p className='p-description'>{currentPassage?.description}</p>
                                 <div className='question-container'>
                                     {/* map loops over the array of question to determine how many questions does the current page have */}
@@ -148,7 +234,7 @@ function IdentifyingInformation() {
                                     </React.Fragment>
                                     )}
                                     {indexOfLastQuestion >= 10 ? (
-                                        <button onClick={() => console.log("Final Answers:", userAnswers)} className='submit-btn-test'>Submit Test</button>
+                                        <button onClick={sendUserAnswers} className='submit-btn-test'>Submit Test</button>
                                     ) : (
                                         <button onClick={handleNextPage} className='next-page-btn'>Next Page</button>
                                     )}
