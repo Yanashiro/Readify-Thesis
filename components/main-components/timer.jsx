88d60def5f405d356b4d37147d8217817094ee4d
SolarCyclone <190097766+SolarCyclone@users.noreply.github.com>
2026-01-29 22:16:17 +0800

    Update timer.jsx

--- components/main-components/timer.jsx
+++ components/main-components/timer.jsx
@@ -1,33 +1,42 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 
-function TimerStart() {
-    const [time, setTime] = useState(900);
+function SideTimer({time, setTime}) {
 
     useEffect(() => {
         let timer = setInterval(() => {
-            setTime((time) => {
-                if (time === 0) {
+            setTime((prevTime) => {
+                if (prevTime === 0) {
                     clearInterval(timer);
+                    window.location.replace('/maintest/timerisdown');
+                    sessionStorage.removeItem("Answer")
+                    sessionStorage.removeItem("Font Size")
+                    sessionStorage.removeItem("Passage History")
+                    sessionStorage.removeItem("Page History")
+                    sessionStorage.removeItem("Questions History")
+                    sessionStorage.removeItem("Timer remain")
                     return 0;
-                } else return time - 1;
+                } else { 
+                    return prevTime - 1};
             });
-        }, 1000)
-    },[]);
+        }, 1000);
+
+        return () => clearInterval(timer);
+    },[setTime]);
+
+    useEffect(() => {
+        if (time !== undefined) {
+            sessionStorage.setItem("Timer remain", JSON.stringify(time));
+        }
+    }, [time]);
 
     return (
         <div className="Timer">
             <p>
-                Time left: {`${Math.floor(time / 60)}`.padStart(2, 0)}: 
+                Timer: {`${Math.floor(time / 60)}`.padStart(2, 0)}: 
                 {`${time % 60}`.padStart(2, 0)}
             </p>
         </div>
-    );
-}
-
-function SideTimer() {
-    return (
-        <TimerStart />
     );
 }
 
