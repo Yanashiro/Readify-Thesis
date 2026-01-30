0d6835bfda2f1bb49c502b02ad16908e64112ba8
SolarCyclone <190097766+SolarCyclone@users.noreply.github.com>
2026-01-30 19:43:34 +0800

    Update matchinginformation.jsx

--- components/practice-test/matchinginformation.jsx
+++ components/practice-test/matchinginformation.jsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import SideTimer from '../main-components/timer';
 import { useCookies } from 'react-cookie';
-import './maintestpage.css'
 
 function MatchingInformation() {
 
