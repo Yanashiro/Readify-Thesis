8eaf65c6814725b59af23a1c69698cc9bc459eca
SolarCyclone <190097766+SolarCyclone@users.noreply.github.com>
2026-01-30 19:37:57 +0800

    Update multiplechoices.jsx

--- components/practice-test/multiplechoices.jsx
+++ components/practice-test/multiplechoices.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import SideTimer from './timer'
+import SideTimer from '../main-components/timer'
 import { useCookies } from 'react-cookie'
 import './maintestpage.css'
 
