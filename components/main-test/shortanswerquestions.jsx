c42d6004e6dcc15b20cc1ac38bb2a2d0f6b095ff
SolarCyclone <190097766+SolarCyclone@users.noreply.github.com>
2026-01-30 19:42:12 +0800

    Update shortanswerquestions.jsx

--- components/main-test/shortanswerquestions.jsx
+++ components/main-test/shortanswerquestions.jsx
@@ -5,7 +5,6 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import SideTimer from '../main-components/timer';
 import { useCookies } from 'react-cookie';
-import './maintestpage.css'
 
 function ShortAnswerQuestions() {
 
