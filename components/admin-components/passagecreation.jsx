e0f2952974fe1aafa8ae809b1cc656b66740ee58
SolarCyclone <190097766+SolarCyclone@users.noreply.github.com>
2026-02-03 18:38:16 +0800

    Create passagecreation.jsx

---
+++ components/admin-components/passagecreation.jsx
@@ -0,0 +1,17 @@
+// still under construction
+
+import react from 'react';
+import axios from 'axios';
+import './passagecreation.css';
+
+function PassageCreation() {
+    return (
+        <>
+            <main className='passagecreation'>
+                
+            </main>
+        </>
+    )
+}
+
+export default PassageCreation;
