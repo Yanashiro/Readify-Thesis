4dcbc0daf63af3f35981dd5368132c660192aee2
SolarCyclone <190097766+SolarCyclone@users.noreply.github.com>
2026-02-03 18:41:30 +0800

    Update identifyinginformation.jsx

--- components/main-test/identifyinginformation.jsx
+++ components/main-test/identifyinginformation.jsx
@@ -44,7 +44,7 @@ function IdentifyingInformation() {
     useEffect(() => {
         if (passageHistory.length === 0) {
         axios
-            .post('/maintestroute/identifyinginformation')
+            .post('/maintestroute/identifyinginformation', {randomize: true})
             .then((res) => {
                 console.log("Number of question received", res.data.questions.length);
                 console.log("Questions Array:", res.data.questions);
@@ -64,8 +64,11 @@ function IdentifyingInformation() {
         sessionStorage.setItem("Passage History", JSON.stringify(passageHistory));
         sessionStorage.setItem("Page History", JSON.stringify(currentPage));
         sessionStorage.setItem("Questions History", JSON.stringify(allQuestions));
+    }, [userAnswers, fontSize, currentPage, allQuestions, passageHistory]);
+
+    useEffect(() => {
         sessionStorage.setItem("Timer remain", time)
-    }, [userAnswers, fontSize, currentPage, allQuestions, passageHistory, time]);
+    }, [time])
 
     const userChoiceClick = (questionId, choiceValue) => {
         // used to save user choices (answers) even if the page is moved
@@ -109,7 +112,7 @@ function IdentifyingInformation() {
             // if length of allQuestions array is greater than variable totalLimit, function returns nothing
             return; // stops the function
         }
-            axios.post('/maintestroute/identifyinginformation')
+            axios.post('/maintestroute/identifyinginformation', {randomize: true})
                 .then((res) => {
                     setAllQuestions(prevQuestions => {
                         const combined = [...prevQuestions, ...res.data.questions]
@@ -128,12 +131,14 @@ function IdentifyingInformation() {
         
         const submissionData = {
             examinee: cookies['examinee-cookie'],
-            answers: userAnswers,
-            data: new Date()
+            testType: "Main",
+            testCategory: "Identifying Information",
+            submittedAnswers: userAnswers,
+            testDate: new Date()
         };
         
         axios
-            .post('/maintestroute/identifyinginformation', submissionData)
+            .post('/maintestroute/examSubmission', submissionData)
             .then((res) => {
                 if (res.status == 200) {
                     window.location.replace('/maintest/examsubmitted');
@@ -207,7 +212,7 @@ function IdentifyingInformation() {
                                     {/* map loops over the array of question to determine how many questions does the current page have */}
                                     {currentQuestions.map((q, index) => (
                                         // the question container - keys make the array of questions individually unique based on the "id" from the backend
-                                        <div className='question-block' key={q.id || index}>
+                                        <div className='question-block' key={q.questionNumber || index}>
                                             {/*  */} 
                                             <p className='questions'><strong>{indexOfFirstQuestion + index + 1}.</strong> {q.text || q.questionText}</p>
                                             <div className='options-list'>
@@ -229,14 +234,14 @@ function IdentifyingInformation() {
                                 <div className='next-back-buttons'>
                                     {currentPage > 0 && (
                                     <React.Fragment>
-                                        <button onClick={() => setCurrentPage(prev => prev - 1)} className='back-btn'>Back</button>
+                                        <button onClick={() => setCurrentPage(prev => prev - 1)} className='back-btn-test'>〈 Back</button>
                                         <br/>
                                     </React.Fragment>
                                     )}
                                     {indexOfLastQuestion >= 10 ? (
                                         <button onClick={sendUserAnswers} className='submit-btn-test'>Submit Test</button>
                                     ) : (
-                                        <button onClick={handleNextPage} className='next-page-btn'>Next Page</button>
+                                        <button onClick={handleNextPage} className='next-page-btn-test'>Next Page 〉</button>
                                     )}
                                 </div>
                             </div>
