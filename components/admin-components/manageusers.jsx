30bc535e921a73eee95a096c58d55dc7edb75293
SolarCyclone <190097766+SolarCyclone@users.noreply.github.com>
2026-02-03 18:37:33 +0800

    Create manageusers.jsx

---
+++ components/admin-components/manageusers.jsx
@@ -0,0 +1,13 @@
+// still under construction
+
+import React from 'react'
+import axios from 'axios'
+import './manageusers.css'
+
+function ManageUsers() {
+    return (
+        <></>
+    )
+}
+
+export default ManageUsers
