4f2aa830a18569050c31ff4248fa864ae3613903
SolarCyclone <190097766+SolarCyclone@users.noreply.github.com>
2026-02-03 18:36:55 +0800

    Create adminhome.jsx

---
+++ components/admin-components/adminhome.jsx
@@ -0,0 +1,16 @@
+// still under construction
+import React from 'react'
+import axios from 'axios'
+import './adminhome.css'
+
+function AdminHome(setPage) {
+    return (
+        <>
+            <main className='adminhome'>
+
+            </main>
+        </>
+    )
+}
+
+export default AdminHome;
