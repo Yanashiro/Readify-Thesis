eaab526f5350c7026684c1a33696369e791273a5
SolarCyclone <190097766+SolarCyclone@users.noreply.github.com>
2026-02-03 18:27:30 +0800

    Update achievements.jsx

--- components/main-components/achievements.jsx
+++ components/main-components/achievements.jsx
@@ -10,6 +10,7 @@ import ieltsStar from '../../images/IELTS_star.png'
 import perfectScore from '../../images/perfect_score.png'
 import elite from '../../images/elite_achiever.png'
 import './achievements.css'
+import axios from 'axios'
 
 function Achievements() {
 
@@ -23,19 +24,14 @@ function Achievements() {
         perfectScore: false,
         elite: false 
     });
-    // needs revision to here, handleUnlock must set setUnlocked useState to update achievement tabs
+    // I'm not sure how fetch works
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch("/api/achievements");
-                const json = await response.json();
-                setUnlocked(json);
-            } catch (error) {
-                console.error(error);
-            } finally {
-                setUnlocked(false);
-            }
-        }
+        axios
+            .post('/achievements')
+            .then((res) => {
+                setUnlocked(res.data)
+            })
+            .catch((err) => console.error(err))
     },[unlocked]) 
 
 
@@ -48,62 +44,62 @@ function Achievements() {
 
     return (
         <>
-            <div>
+            <div className='achievements-title'>
                 <h1>Achievements</h1>
             </div>
             <div>
                 <div className="container" onClick={() => handleUnlock('firstSteps')}>
-                    <img src={unlocked.firstSteps ? firstSteps : lockedImage} width={"50px"} height={"50px"}></img>
-                    <div>
+                    <img src={unlocked.firstSteps ? firstSteps : lockedImage} width={"80px"} height={"80px"}></img>
+                    <div className='achievements-name-center'>
                         <p className="p-b"><b>First Steps</b></p>
                         <p className="p-a">Completed first reading test</p>
                     </div>
                 </div>
                 <div className="container" onClick={() => handleUnlock('getAHang')}>
-                    <img src={unlocked.getAHang ? getAHang : lockedImage} width={"50px"} height={"50px"}></img>
-                    <div>
+                    <img src={unlocked.getAHang ? getAHang : lockedImage} width={"80px"} height={"80px"}></img>
+                    <div className='achievements-name-center'>
                         <p className="p-b"><b>Getting the Hang of it</b></p>
                         <p className="p-a">Completed 5 tests</p>
                     </div>
                 </div>
                 <div className="container" onClick={() => handleUnlock('marathon')}>
-                    <img src={unlocked.marathon ? marathon : lockedImage} width={"50px"} height={"50px"}></img>
-                    <div>
+                    <img src={unlocked.marathon ? marathon : lockedImage} width={"80px"} height={"80px"}></img>
+                    <div className='achievements-name-center'>
                         <p className="p-b"><b>Marathon Reader</b></p>
                         <p className="p-a">Completed 10 tests</p>
                     </div>
                 </div>
                 <div className="container" onClick={() => handleUnlock('comprehensionStart')}>
-                    <img src={unlocked.comprehensionStart ? comprehensionStart : lockedImage} width={"50px"} height={"50px"}></img>
-                    <div>
+                    <img src={unlocked.comprehensionStart ? comprehensionStart : lockedImage} width={"80px"} height={"80px"}></img>
+                    <div className='achievements-name-center'>
                         <p className="p-b"><b>Comprehension Starter</b></p>
                         <p className="p-a">Scored 50% or higher in a test</p>
                     </div>
                 </div>
                 <div className="container" onClick={() => handleUnlock('sharpRead')}>
-                    <img src={unlocked.sharpRead ? sharpRead : lockedImage} width={"50px"} height={"50px"}></img>
-                    <div>
+                    <img src={unlocked.sharpRead ? sharpRead : lockedImage} width={"80px"} height={"80px"}></img>
+                    <div className='achievements-name-center'>
                         <p className="p-b"><b>Sharp Reader</b></p>
                         <p className="p-a">Scored 75% or higher on any test</p>
                     </div>
                 </div>
                 <div className="container" onClick={() => handleUnlock('ieltsStar')}>
-                    <img src={unlocked.ieltsStar ? ieltsStar : lockedImage} width={"50px"} height={"50px"}></img>
-                    <div>
+                    <img src={unlocked.ieltsStar ? ieltsStar : lockedImage} width={"80px"} height={"80px"}></img>
+                    <div className='achievements-name-center'>
                         <p className="p-b"><b>IELTS Star</b></p>
                         <p className="p-a">Scored 90% or higher on any test</p>
                     </div>
                 </div>
                 <div className="container" onClick={() => handleUnlock('perfectScore')}>
-                    <img src={unlocked.perfectScore ? perfectScore : lockedImage} width={"50px"} height={"50px"}></img>
-                    <div>
+                    <img src={unlocked.perfectScore ? perfectScore : lockedImage} width={"80px"} height={"80px"}></img>
+                    <div className='achievements-name-center'>
                         <p className="p-b"><b>Perfect Score</b></p>
                         <p className="p-a">Scored 100% in one test</p>
                     </div>
                 </div>
                 <div className="container" onClick={() => handleUnlock('elite')}>
-                    <img src={unlocked.elite ? elite : lockedImage} width={"50px"} height={"50px"}></img>
-                    <div>
+                    <img src={unlocked.elite ? elite : lockedImage} width={"80px"} height={"80px"}></img>
+                    <div className='achievements-name-center'>
                         <p className="p-b"><b>Elite Achiever</b></p>
                         <p className="p-a">Unlocked all achievements</p>
                     </div>
