ab0f1dbca0627d91243747d84cd797f89dce440c
SolarCyclone <190097766+SolarCyclone@users.noreply.github.com>
2026-02-03 18:46:26 +0800

    Update summarycompletion.jsx

--- components/main-test/summarycompletion.jsx
+++ components/main-test/summarycompletion.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import SideTimer from '../main-components/timer';
 import { useCookies } from 'react-cookie';
-import './maintestpage.css';
+import './maintestpage.css'
 
 function SummaryCompletion() {
 
@@ -50,7 +50,7 @@ function SummaryCompletion() {
     useEffect(() => {
         if (passageHistory.length === 0) {
         axios
-            .post('/maintestroute/summarycompletion')
+            .post('/maintestroute/summarycompletion', {randomize: true})
             .then((res) => {
                 console.log("Number of question received", res.data.questions.length);
                 console.log("Questions Array:", res.data.questions);
@@ -72,8 +72,11 @@ function SummaryCompletion() {
         sessionStorage.setItem("Page History", JSON.stringify(currentPage));
         sessionStorage.setItem("Questions History", JSON.stringify(allQuestions));
         sessionStorage.setItem("Summary History", JSON.stringify(summaryHistory))
+    }, [userAnswers, fontSize, currentPage, allQuestions, passageHistory, summaryHistory]);
+
+    useEffect(() => {
         sessionStorage.setItem("Timer remain", time)
-    }, [userAnswers, fontSize, currentPage, allQuestions, passageHistory, time, summaryHistory]);
+    }, [time])
 
     const userWriteDown = (questionId, writeValue) => {
         // setUserAnswers is initiated as "prev" parameter that saves previously answered questions 
@@ -119,7 +122,7 @@ function SummaryCompletion() {
         }
         // requesting data from the backend every "Next Page" click
         axios
-            .post('/maintestroute/summarycompletion')
+            .post('/maintestroute/summarycompletion', {randomize: true})
             .then((res) => {
                 setAllQuestions(prevQuestions => {
                     // setAllQuestions was initiated as prevQuestions parameter "..." means all previous following data, 
@@ -146,12 +149,14 @@ function SummaryCompletion() {
         
         const submissionData = {
             examinee: cookies['examinee-cookie'],
-            answers: userAnswers,
-            data: new Date()
+            testType: "Main",
+            testCategory: "Summary Completion",
+            submittedAnswers: userAnswers,
+            testDate: new Date()
         };
         
         axios
-            .post('/maintestroute/summarycompletion', submissionData)
+            .post('/maintestroute/examSubmission', submissionData)
             .then((res) => {
                 if (res.status == 200) {
                     window.location.replace('/maintest/examsubmitted');
@@ -180,7 +185,7 @@ function SummaryCompletion() {
                     <h3 className='sidetimer-h2'><SideTimer time={time} setTime={setTime}/></h3>
                 </div>
                 <div className='warning-tab'>
-                    <p className='warning-text'>Warning! Questions are Randomized. Multiple<br /> tab changes can result in exam <br />termination. Do not <br/> refresh the page or <br/> your data resets</p>
+                    <p className='warning-text'>Warning! Questions are Randomized. Multiple<br /> tab changes can result in exam <br />termination.</p>
                 </div>
             </section>
             <div className='section-flex'>
@@ -226,7 +231,7 @@ function SummaryCompletion() {
                                 <div className='question-container'>
                                     {/*  */}
                                     {currentQuestions.map((q, index) => (
-                                        <div className='question-block-summary' key={q.id || index}>
+                                        <div className='question-block-summary' key={q.questionNumber || index}>
                                             <p className='questions-summary'><strong>{indexOfFirstQuestion + index + 1}.</strong></p>
                                             <input 
                                                 type='text'
@@ -241,14 +246,14 @@ function SummaryCompletion() {
                                 <div className='next-back-buttons'>
                                     {currentPage > 0 && (
                                     <React.Fragment>
-                                        <button onClick={() => setCurrentPage(prev => prev - 1)} className='back-btn'>Back</button>
+                                        <button onClick={() => setCurrentPage(prev => prev - 1)} className='back-btn-test'>〈 Back</button>
                                         <br/>
                                     </React.Fragment>
                                     )}
                                     {indexOfLastQuestion >= 10 ? (
                                         <button onClick={sendUserAnswers} className='submit-btn-test'>Submit Test</button>
                                     ) : (
-                                        <button onClick={handleNextPage} className='next-page-btn'>Next Page</button>
+                                        <button onClick={handleNextPage} className='next-page-btn-test'>Next Page 〉</button>
                                     )}
                                 </div>
                             </div>
