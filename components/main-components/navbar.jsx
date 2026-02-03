18dda4b5a9a56787f02fba240d3caf6335358f52
SolarCyclone <190097766+SolarCyclone@users.noreply.github.com>
2026-02-03 18:31:09 +0800

    Update navbar.jsx

--- components/main-components/navbar.jsx
+++ components/main-components/navbar.jsx
@@ -20,49 +20,80 @@ import TipsnTricks from './tipsntricks';
 import AboutUs from './aboutus';
 import Achievements from './achievements';
 import Profile from './profile';
+import AdminHome from '../admin-components/adminhome';
+import PassageCreation from '../admin-components/passagecreation';
+import ManageUsers from '../admin-components/manageusers';
+import TestReview from '../admin-components/testreview';
 //import dropdownIcon from '../images/icons/dropdown.png';
 
-function PageNavigation({input}) {
+function PageNavigation({input, setInput}) {
+  const [cookies, setCookie, removeCookie] = useCookies(['examinee-cookie', 'admin-cookie'])
+  const isAdmin = !!cookies['admin-cookie']
+  const isExaminee = !!cookies['examinee-cookie']
     
+  if (isExaminee) {
     switch(input) {
-        case 'Dashboard':
-            return <Dashboard/>
-        case 'Main Test':
-            return <MTPage/>
-        case 'Practice Test':
-            return <PTPage/>  
-        case 'Achievements':
-            return <Achievements/>
-        case 'Tips & Tricks':
-            return <TipsnTricks/>
-        case 'About Us':
-            return <AboutUs/>
-        case 'Profile':
-            return <Profile/>
-        default:
-            return <Dashboard/>
+      case 'Dashboard':
+          return <Dashboard setPage={setInput}/>
+      case 'Main Test':
+          return <MTPage/>
+      case 'Practice Test':
+          return <PTPage/>  
+      case 'Achievements':
+          return <Achievements/>
+      case 'Tips & Tricks':
+          return <TipsnTricks/>
+      case 'About Us':
+          return <AboutUs/>
+      case 'Profile':
+          return <Profile/>
+      default:
+          return <Dashboard/>
     }
+  } else if (isAdmin) {
+    switch(input) {
+      case 'Home':
+        return <AdminHome setPage={setInput}/>
+      case 'Passage Creation':
+        return <PassageCreation/>
+      case 'Manage Users':
+        return <ManageUsers setPage={setInput}/>
+      case 'Test Review':
+        return <TestReview/>
+      default:
+        return <AdminHome/>
+    }
+  }
+
+  return null;
 }
 
 function Navbar() {
-  const [cookies, , removeCookie] = useCookies(['examinee-cookie'])
-  const [activeMenu, setActiveMenu] = useState(() => {
-    const currentPath = window.location.pathname;
-
-    if(currentPath === '/home') {
-      return 'Dashboard';
+  const [cookies, setCookie, removeCookie] = useCookies(['examinee-cookie', 'admin-cookie'])
+  const isAdmin = !!cookies['admin-cookie']
+  const isExaminee = !!cookies['examinee-cookie']
+  const [activeMenu, setActiveMenu] = useState(() => { 
+    if(isExaminee) {
+      const currentPath = window.location.pathname;
+      if(currentPath === '/home') {
+        return 'Dashboard';
+      }
+      return ''
+    } else if (isAdmin) {
+      const currentPath = window.location.pathname;
+      if(currentPath === '/home') {
+        return 'Home';
+      }
+      return ''
     }
-    return ''});
+  });
 
   const handleMenuClick = (menuName) => {
     setActiveMenu(menuName);
   }
-
-useEffect(() => {
-    // function to clear sessionStorage of exams
-    const clearExamData = () => {
-    // object collection of all sessionStorage
-    const examKeys = [
+  {/*}
+  useEffect(() => {
+      const currentExamData = [
         "Answer", 
         "Font Size", 
         "Passage History", 
@@ -73,38 +104,26 @@ useEffect(() => {
         "Timer remain",
         "Headings History",
         "Summary History"
-    ]; 
-    examKeys.forEach(key => sessionStorage.removeItem(key));
-    console.log("Storage cleared: User returned to Home/Category selection.");
-    };
-    // testing feature if its not on main test anymore (not working, needs reload)
-    if (!window.location.pathname.includes('/maintest/*')) {
-      // attempt clearing exam data (unsuccessful)
-    clearExamData();
-    }
-    // once the page is redirected
-    window.addEventListener('pageshow', (event) => {
-    if (event.persisted && !window.location.pathname.includes('/maintest/*')) {
-        // clear exam data function
-        clearExamData();
-        // reload to ensure the sessionStorage updates
-        if (window.location.reload) window.location.reload(); 
-    }
-    });
-    // happens only once every time the page reloads 
-}, [])
+      ];
 
+      if (sessionStorage.getItem("Timer remain")) {
+        currentExamData.forEach(key => sessionStorage.removeItem(key));
+        console.warn("Anti-Cheat: Progress wiped.");
+        window.location.reload();
+      }
+  }, []) */}
   useEffect(() => {
-    if(!cookies['examinee-cookie']) {
+    if(!cookies['examinee-cookie' || 'admin-cookie']) {
       alert("User Identity lost, logging out");
       sessionStorage.clear();
       window.location.replace('/');
     }
-  }, [cookies['examinee-cookie']]);
+  }, [cookies['examinee-cookie'], cookies['admin-cookie']]);
 
   const handleLogout = () => {
     if (window.confirm("Are you sure you want to log out?")) {
       removeCookie('examinee-cookie', {path: '/'});
+      removeCookie('admin-cookie', {path:"/"});
       sessionStorage.clear();
       window.location.replace('/');
     }
@@ -121,20 +140,20 @@ useEffect(() => {
 
         <div className="user-dropdown">
           <img src={userIcon} className="user-nav-icon" alt="User" />
-          <span>{cookies['examinee-cookie'] || "User"}</span>
+          <span>{isExaminee ? cookies['examinee-cookie'] : isAdmin ? cookies['admin-cookie'] : 'User'}</span>
           {/*<img src={dropdownIcon} className="dropdown-icon" alt="Dropdown" /> */}
         </div>
       </div>
-
       {/* MAIN CONTAINER */}
       <div className="main-container">
         {/* SIDEBAR */}
+        {isExaminee &&
         <div className="sidebar-nav">
           <div className="sidebar-title">All Tests</div>
           <a 
             className={`menu-item ${activeMenu === 'Dashboard' ? 'active' : ''}`} 
-            onClick={() => handleMenuClick('Dashboard')}
-            href="#!"
+            onClick={() => handleMenuClick('Dashboard')} 
+            href="#"
           > 
             <img src={dashboardIcon} className="menu-icon" alt="Dashboard" />
             <span>Dashboard</span>
@@ -142,7 +161,7 @@ useEffect(() => {
           <a 
             className={`menu-item ${activeMenu === 'Main Test' ? 'active' : ''}`} 
             onClick={() => handleMenuClick('Main Test')}
-            href="#!"
+            href="#"
           >
             <img src={mainTestIcon} className="menu-icon" alt="Main Test" />
             <span>Main Test</span>
@@ -151,7 +170,7 @@ useEffect(() => {
           <a 
             className={`menu-item ${activeMenu === 'Practice Test' ? 'active' : ''}`} 
             onClick={() => handleMenuClick('Practice Test')}
-            href="#!"
+            href="#"
           >
             <img src={practiceTestIcon} className="menu-icon" alt="Practice Test" />
             <span>Practice Test</span>
@@ -160,7 +179,7 @@ useEffect(() => {
           <a 
             className={`menu-item ${activeMenu === 'Achievements' ? 'active' : ''}`} 
             onClick={() => handleMenuClick('Achievements')}
-            href="#!"
+            href="#"
           >
             <img src={achievementIcon} className="menu-icon" alt="Achievements" />
             <span>Achievements</span>
@@ -169,27 +188,18 @@ useEffect(() => {
           <a 
             className={`menu-item ${activeMenu === 'Tips & Tricks' ? 'active' : ''}`} 
             onClick={() => handleMenuClick('Tips & Tricks')}
-            href="#!"
+            href="#"
           >
             <img src={tipsIcon} className="menu-icon" alt="Tips & Tricks" />
             <span>Tips & Tricks</span>
           </a>
 
           <div className="settings-separator">Settings</div>
-        {/*
-          <a 
-            className={`menu-item ${activeMenu === 'Settings' ? 'active' : ''}`} 
-            onClick={() => handleMenuClick('Settings')}
-            href="#!"
-          >
-            <img src={settingsIcon} /*className="menu-icon" alt="Settings" />
-            <span>Settings</span>
-          </a> */}
 
           <a 
             className={`menu-item ${activeMenu === 'Profile' ? 'active' : ''}`} 
             onClick={() => handleMenuClick('Profile')}
-            href="#!"
+            href="#"
           >
             <img src={profileIcon} className="menu-icon" alt="Profile" />
             <span>Profile</span>
@@ -198,7 +208,7 @@ useEffect(() => {
           <a 
             className={`menu-item ${activeMenu === 'About Us' ? 'active' : ''}`} 
             onClick={() => handleMenuClick('About Us')}
-            href="#!"
+            href="#"
           >
             <img src={aboutUsIcon} className="menu-icon" alt="About Us" />
             <span>About Us</span>
@@ -211,17 +221,55 @@ useEffect(() => {
             </a>
           </div>
         </div>
+      }
 
+      {isAdmin &&
+        <div className="sidebar-nav">
+          <div className="sidebar-title">All Tests</div>
+          <a 
+            className={`menu-item ${activeMenu === 'Home' ? 'active' : ''}`} 
+            onClick={() => handleMenuClick('Home')} 
+            href="#"
+          > 
+            <img src={dashboardIcon} className="menu-icon" alt="Home" />
+            <span>Home</span>
+          </a> 
+
+          <a 
+            className={`menu-item ${activeMenu === 'Passage Creation' ? 'active' : ''}`} 
+            onClick={() => handleMenuClick('Passage Creation')} 
+            href="#"
+          > 
+            <img src={mainTestIcon} className="menu-icon" alt="Passage Creation" />
+            <span>Passage Creation</span>
+          </a> 
+
+          <a 
+            className={`menu-item ${activeMenu === 'Manage Users' ? 'active' : ''}`} 
+            onClick={() => handleMenuClick('Manage Users')} 
+            href="#"
+          > 
+            <img src={practiceTestIcon} className="menu-icon" alt="Manage Users" />
+            <span>Manage Users</span>
+          </a> 
+
+          <a 
+            className={`menu-item ${activeMenu === 'Test Review' ? 'active' : ''}`} 
+            onClick={() => handleMenuClick('Test Review')} 
+            href="#"
+          > 
+            <img src={practiceTestIcon} className="menu-icon" alt="Test Review" />
+            <span>Test Review</span>
+          </a> 
+        </div>
+      }
         {/* MAIN CONTENT */}
         <div className="main-content">
-            <PageNavigation input={activeMenu} />
+            <PageNavigation input={activeMenu} setInput={setActiveMenu} />
         </div>
       </div>
     </div>
   );
-}
-
-export default Navbar;
 }
 
 export default Navbar;
