902e95e0de264c3f7f4bfe5d34a3aa95283d0fe4
SolarCyclone <190097766+SolarCyclone@users.noreply.github.com>
2026-02-03 18:38:54 +0800

    Create testreview.jsx

---
+++ components/admin-components/testreview.jsx
@@ -0,0 +1,13 @@
+// still under construction
+
+import React from 'react'
+import axios from 'axios'
+import './testreview.css'
+
+function TestReview() {
+    return (
+        <></>
+    )
+}
+
+export default TestReview;
