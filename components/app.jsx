4b4f9b409c07c0d34cb6e724423d05cdcad9dd64
SolarCyclone <190097766+SolarCyclone@users.noreply.github.com>
2026-02-07 15:57:51 +0800

    Create app.jsx

---
+++ components/app.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from './main-components/navbar'
+import Login from './main-components/login'
+import Signup from './main-components/signup'
+import './main-components/maintestboard.css'
+import MainTestRoute from './maintestrouter'
+import PracticeTestRoute from './practicetestrouter'
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<Login/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/signup" element={<Signup/>}/>         
+            <Route path="/home" element={<Navbar/>}/>
+
+            <Route path='/maintest/*' element={<MainTestRoute/>}/>
+            <Route path='/practicetest/*' element={<PracticeTestRoute/>}/>
+        </Routes>
+    )
+}
+
+export default App;
