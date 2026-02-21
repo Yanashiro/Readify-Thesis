4cdc63f2385efdf50d1ae9be2100126e1a44e2cd
SolarCyclone <190097766+SolarCyclone@users.noreply.github.com>
2026-02-21 15:23:02 +0800

    Update achievements.jsx

--- components/main-components/achievements.jsx
+++ components/main-components/achievements.jsx
@@ -17,44 +17,35 @@ function Achievements() {
     const [unlocked, setUnlocked] = useState([]);
 
     const achievementList = [
-        {id: 'firstSteps', name: 'First Steps', desc: 'Completed first reading test', img: firstSteps},
-        {id: 'getAHang', name: 'Getting the Hang of it', desc: 'Completed 5 tests', img: getAHang},
-        {id: 'marathon', name: 'Marathon Reader', desc: 'Completed 10 tests', img: marathon},
-        {id: 'comprehensionStart', name: 'Comprehension Starter', desc: 'Scored 50% or higher in a test', img: comprehensionStart},
-        {id: 'sharpRead', name: 'Sharp Reader', desc: 'Scored 75% or higher in a test', img: sharpRead },
-        {id: 'ieltsStar', name: 'IELTS Star', desc: 'Scored 90% or higher in a test', img: ieltsStar},
-        {id: 'perfectScore', name: 'Perfect Score', desc: 'Scored 100% in one test', img: perfectScore},
-        {id: 'elite', name: 'Elite Achiever', desc: 'Unlocked all achievements', img: elite}
+        {title: 'firstSteps', name: 'First Steps', desc: 'Completed first reading test', img: firstSteps},
+        {title: 'getAHang', name: 'Getting the Hang of it', desc: 'Completed 5 tests', img: getAHang},
+        {title: 'marathon', name: 'Marathon Reader', desc: 'Completed 10 tests', img: marathon},
+        {title: 'comprehensionStart', name: 'Comprehension Starter', desc: 'Scored 50% or higher in a test', img: comprehensionStart},
+        {title: 'sharpRead', name: 'Sharp Reader', desc: 'Scored 75% or higher in a test', img: sharpRead },
+        {title: 'ieltsStar', name: 'IELTS Star', desc: 'Scored 90% or higher in a test', img: ieltsStar},
+        {title: 'perfectScore', name: 'Perfect Score', desc: 'Scored 100% in one test', img: perfectScore},
+        {title: 'elite', name: 'Elite Achiever', desc: 'Unlocked all achievements', img: elite}
     ]
 
     useEffect(() => {
         axios
-            .post('/achievements')
+            .get('/achievements', { withCredentials: true })
             .then((res) => {
                 setUnlocked(res.data)
             })
             .catch((err) => console.error(err))
     },[]) 
 
-    /*
-    const handleUnlock = (key) => {
-        setUnlocked(prev => ({
-            ...prev,
-            [key]: true
-        }));
-    };
-    */
-
-
     return (
         <>
+            <main className='achievements-main'>
             <div className='achievements-title'>
                 <h1>Achievements</h1>
             </div>
             <div>
                 {achievementList.map((ach) => (
-                <div className="container" key={ach.id}>
-                    <img src={unlocked[ach.id] ? ach.img : lockedImage} width={"80px"} height={"80px"} alt={ach.name}></img>
+                <div className="container" key={ach.title}>
+                    <img src={unlocked[ach.title] ? ach.img : lockedImage} width={"80px"} height={"80px"} alt={ach.name}></img>
                     <div className='achievements-name-center'>
                         <p className="p-b"><b>{ach.name}</b></p>
                         <p className="p-a">{ach.desc}</p>
@@ -62,8 +53,10 @@ function Achievements() {
                 </div>
                 ))}
             </div>
+            </main>
         </>
     );
 }
 
 export default Achievements;
+
