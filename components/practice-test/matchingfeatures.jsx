2ef8cb172a219de0189ceafb21b8a13e865768d7
SolarCyclone <190097766+SolarCyclone@users.noreply.github.com>
2026-02-21 15:06:34 +0800

    Update matchingfeatures.jsx

--- components/practice-test/matchingfeatures.jsx
+++ components/practice-test/matchingfeatures.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import SideTimer from '../main-components/timer';
 import axios from 'axios';
-import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
 import './practicetestpage.css'
 
 function MatchingFeatures() {
 
-    const [cookies] = useCookies(['examinee-cookie'])
+    const [showPopup, setShowPopup] = useState(false);
     const [userAnswers, setUserAnswers] = useState(() => {
         const saved = sessionStorage.getItem("Answer");
         return saved ? JSON.parse(saved) : {}});
@@ -22,27 +22,37 @@ function MatchingFeatures() {
         return saved ? JSON.parse(saved) : []});
     const [featuresHistory, setFeaturesHistory] = useState(() => {
         const saved = sessionStorage.getItem("Features History")
-        return saved ? JSON.parse(saved) : {}});
+        return saved ? JSON.parse(saved) : []});
     const [fontSize, setFontSize] = useState(() => {
         const saved = sessionStorage.getItem("Font Size");
-        return saved ? JSON.parse(saved) : 20});;
+        return saved ? JSON.parse(saved) : 20});
     const [time, setTime] = useState(() => {
         const saved = sessionStorage.getItem("Timer remain");
-        return saved ? JSON.parse(saved) : 900})
+        return saved ? JSON.parse(saved) : 540});
+    const [passageId, setPassageId] = useState(() => {
+        const saved = sessionStorage.getItem("Passage ID");
+        return saved ? JSON.parse(saved) : null;
+    })
 
     const questionsPerPage = 3;
     const indexOfLastQuestion = (currentPage + 1) * questionsPerPage;
     const indexOfFirstQuestion = indexOfLastQuestion - questionsPerPage; 
-    const currentQuestions = allQuestions.slice(indexOfFirstQuestion, indexOfLastQuestion);
+    const currentQuestions = (allQuestions || []).slice(indexOfFirstQuestion, indexOfLastQuestion);
     const currentPassage = passageHistory[currentPage];
-    const currentFeatures = featuresHistory[currentPage];
+    const currentFeatures = featuresHistory[currentPage] || [];
     const questionNumberStart = indexOfFirstQuestion;
     const questionNumberEnd = indexOfLastQuestion;
 
     useEffect(() => {
         if (passageHistory.length === 0) {
+
+        const queryParams = {
+            designation: 'false',
+            type: 2
+        }
+
         axios
-            .post('/practicetestroute/matchingfeatures', {randomize: true})
+            .get('/start-random-exam', {params: queryParams})
             .then((res) => {
                 console.log("Number of question received", res.data.questions.length);
                 console.log("Questions Array:", res.data.questions);
@@ -51,6 +61,7 @@ function MatchingFeatures() {
                 // taking important details (JSON), set to passageHistory
                 setPassageHistory([res.data]);
                 setFeaturesHistory([res.data.features]);
+                setPassageId(res.data.passageId);
             })
             .catch((err) => console.error(err))
         }
@@ -64,7 +75,8 @@ function MatchingFeatures() {
         sessionStorage.setItem("Features History", JSON.stringify(featuresHistory));
         sessionStorage.setItem("Page History", JSON.stringify(currentPage));
         sessionStorage.setItem("Questions History", JSON.stringify(allQuestions));
-    }, [userAnswers, fontSize, currentPage, allQuestions, passageHistory, featuresHistory]);
+        sessionStorage.setItem("Passage ID", JSON.stringify(passageId));
+    }, [userAnswers, fontSize, currentPage, allQuestions, passageHistory, featuresHistory, passageId]);
 
     useEffect(() => {
         sessionStorage.setItem("Timer remain", time)
@@ -98,7 +110,7 @@ function MatchingFeatures() {
 
     const handleNextPage = () => {
 
-        const totalLimit = 10;
+        const totalLimit = 6;
 
         if(passageHistory.length > currentPage +  1) {
             setCurrentPage(prev => prev + 1)
@@ -107,6 +119,7 @@ function MatchingFeatures() {
         if(allQuestions.length >= totalLimit) {
             return;
         }
+        /*
         axios
             .post('/practicetestroute/matchingfeatures', {randomize: true})
             .then((res) => {
@@ -127,23 +140,38 @@ function MatchingFeatures() {
                 setFeaturesHistory(prev => [...prev, res.data.features]);
             })
             .catch((err) => console.error(err));
+        */
     }
+
+    const typeLabels = {
+        1: "Multiple Choice",
+        2: "Matching Features",
+        3: "Matching Information",
+        4: "Identifying Information",
+        5: "Identifying Writer's Views",
+        6: "Matching Sentence Endings",
+        7: "Matching Headings",
+        8: "Summary Completion",
+        9: "Short Answer Questions",
+        10: "Sentence Completion",
+        11: "Diagram Label Completion",
+    };
 
     const sendUserAnswers = () => {
         
         const submissionData = {
-            examinee: cookies['examinee-cookie'],
             testType: "Practice",
             testCategory: "Matching Features",
-            submittedAnswers: userAnswers,
+            userAnswers: userAnswers,
+            passageId: passageId,
             testDate: new Date()
         };
         
         axios
-            .post('/practicetestroute/examSubmission', submissionData)
+            .post('/submit-results', submissionData, { withCredentials: true})
             .then((res) => {
                 if (res.status == 200) {
-                    window.location.replace('/maintest/examsubmitted');
+                    setShowPopup(true);
                     sessionStorage.removeItem("Answer")
                     sessionStorage.removeItem("Font Size")
                     sessionStorage.removeItem("Passage History")
@@ -157,9 +185,24 @@ function MatchingFeatures() {
                 console.error(err)});
     }
 
+    const navigate = useNavigate();
+    const handleGoBack = () => {
+        navigate("/home")
+    }
+
     return (
         <>
         <main className='main-maintest'>
+            {showPopup && (
+                <div className="popup-overlay">
+                    <div className="popup-content">
+                        <h2>Test Finished</h2>
+                        <button className="popup-btn" onClick={handleGoBack}>
+                            Go back to Main Test
+                        </button>
+                    </div>
+                </div>
+            )}
             <section className='sidebar'>
                 <div>
                     <h1 className='name'>Readify</h1>
@@ -175,7 +218,7 @@ function MatchingFeatures() {
             <div className='section-flex'>
                 <section className='testing-flex'>
                     <div className='title-div'>
-                        <h1 className='h1-title-div'>{currentPassage?.testTitle}</h1>
+                        <h1 className='h1-title-div'>{typeLabels[currentPassage?.testType]}</h1>
                     </div>
                     <div className='view-size-buttons'>
                         <button className='font-size-btn' onClick={decreaseFontSize}>
@@ -192,10 +235,10 @@ function MatchingFeatures() {
                 <div className='two-sections'>
                     <div className='passage-view'>
                         <div className='test-title'>
-                            <>{currentPassage?.title}</>
+                            <>{currentPassage?.passageTitle}</>
                         </div>
                         <div className='test-reference'>
-                            <>{currentPassage?.linkReference}</>
+                            <>{currentPassage?.passageSource}</>
                         </div>
                         <div className='test-passage'>
                             <p style={{fontSize: `${fontSize}px`}}>{currentPassage?.passage}</p>
@@ -204,7 +247,7 @@ function MatchingFeatures() {
                     <section className='questions-side'>
                         <div>
                             <div>
-                                <b><p className='p-questionRange'>Questions {questionNumberStart + 1}{questionNumberEnd <= 10 ? `-${questionNumberEnd}` : ''}</p></b>
+                                <b><p className='p-questionRange'>Questions {questionNumberStart + 1}{questionNumberEnd <= 6 ? `-${questionNumberEnd}` : ''}</p></b>
                                 <p className='p-description'>{currentPassage?.description}</p>
                                 <div className='feature-block'>
                                     <p className='features-font'>Features</p>
@@ -217,13 +260,13 @@ function MatchingFeatures() {
                                 <div className='question-container'>
                                     {currentQuestions.map((q, index) => (
                                         <div className='question-block' key={q.questionNumber || index}>
-                                            <p className='questions'><strong>{indexOfFirstQuestion + index + 1}.</strong> {q.text || q.questionText}</p>
+                                            <p className='questions'><strong>{indexOfFirstQuestion + index + 1}.</strong> {q.questionText}</p>
                                             <div className='options-list display-flex'>
                                                 {(q.options || q.data).map((opt, index2) => (
                                                     <React.Fragment key={opt}>
                                                         <button
-                                                            className={`${userAnswers[q.id || q.questionNumber] === opt ? 'active-opt-letters': 'opt-btn-letters'}`}
-                                                            onClick={() => userChoiceClick(q.id || q.questionNumber, opt)}
+                                                            className={`${userAnswers[q.questionNumber] === opt ? 'active-opt-letters': 'opt-btn-letters'}`}
+                                                            onClick={() => userChoiceClick(q.questionNumber, opt)}
                                                         >
                                                             {opt}
                                                         </button>
