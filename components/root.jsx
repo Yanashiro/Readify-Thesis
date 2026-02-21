5e91c02c83109cfd3de1a8d53fe9f8af29bc27fd
SolarCyclone <190097766+SolarCyclone@users.noreply.github.com>
2026-02-21 15:14:08 +0800

    Update root.jsx

--- components/root.jsx
+++ components/root.jsx
@@ -2,16 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { CookiesProvider } from 'react-cookie'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <CookiesProvider>
                 <App />
-            </CookiesProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
