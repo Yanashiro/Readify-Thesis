e6ea785e6902af78c850b7fc9527d98a1d30cbea
SolarCyclone <190097766+SolarCyclone@users.noreply.github.com>
2026-02-21 15:22:19 +0800

    Update aboutus.jsx

--- components/main-components/aboutus.jsx
+++ components/main-components/aboutus.jsx
@@ -4,6 +4,7 @@ import picture from '../../images/picture_aboutus.png';
 import micah from '../../images/ROCERO_ID_Picture.JPG';
 import patrick from '../../images/patrick-photo.png'
 import marjorie from '../../images/CINCO_ID_PICTURE.jpg';
+import ange from '../../images/ange.jpg';
 import ryan from '../../images/Ryan.jpg'
 
 function AboutUs() {
@@ -11,7 +12,7 @@ function AboutUs() {
         <main className="aboutUsMain">
             <div className='about-us-header'>
                 <h1>About Us</h1>
-                <p>We are the team that built this website. The details shared here are our motivations for this project</p>
+                <p>Readify is an online reading comprehension platform created to help senior high school students improve their reading skills and prepare for college-level academic demands. Based on the IELTS reading framework, Readify provides automated assessments, diverse question types, instant feedback, and progress tracking to support both students and teachers. Our goal is to make reading assessment accessible, promote better reading habits, and help students build strong comprehension skills for academic and real-world success.</p>
             </div>
             <div>
                 <div className='meet-the-team'>
@@ -38,7 +39,7 @@ function AboutUs() {
                     </div>
                 </div>
                 <div className="containerAboutUs">
-                    <img className="picture" src={picture} width={"250px"} height={"250px"}></img>
+                    <img className="picture" src={ange} width={"250px"} height={"250px"}></img>
                     <div className='team-name'>
                         <h2>Angela Dacasin</h2>
                         <p>I am a BS Computer Science student specializing in Data Science with a strong interest in transforming data into meaningful insights. I have hands-on experience in statistical analysis and data visualization, supported by solid analytical and problem-solving skills. I actively engage with the data science community through conferences and seminars to stay updated with emerging trends and best practices.</p>
@@ -73,3 +74,4 @@ function AboutUs() {
 };
 
 export default AboutUs
+
