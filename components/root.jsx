81574a46a37b24f8355994d1c301298655a047ca
SolarCyclone <190097766+SolarCyclone@users.noreply.github.com>
2026-02-24 14:27:44 +0800

    Update root.jsx

--- components/root.jsx
+++ components/root.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './app'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import App from './app';
 
-const root = createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-root.render(
+const RootApp = () => (
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
+
+root.render(<RootApp />);
