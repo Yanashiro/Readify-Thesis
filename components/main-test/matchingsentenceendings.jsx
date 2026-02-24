24ed7271341ca17797275c0878a6901c6d80a43a
SolarCyclone <190097766+SolarCyclone@users.noreply.github.com>
2026-02-24 10:48:19 +0800

    Update matchingsentenceendings.jsx

--- components/main-test/matchingsentenceendings.jsx
+++ components/main-test/matchingsentenceendings.jsx
@@ -54,14 +54,15 @@ function MatchingSentenceEndings() {
         axios
             .post('/start-random-exam', { params: queryParams })
             .then((res) => {
+                console.log("Backend response:", res.data)
                 console.log("Number of question received", res.data.questions.length);
                 console.log("Questions Array:", res.data.questions);
                 // taking all questions from the randomizer (JSON)
-                setAllQuestions(res.data.questions);
+                setAllQuestions(res.data.test.questions);
                 // taking important details (JSON), set to passageHistory
-                setPassageHistory([res.data]);
-                setEndingsHistory([res.data.endings]);
-                setPassageId(res.data.passageId)
+                setPassageHistory([res.data.test]);
+                setEndingsHistory([res.data.test.endings]);
+                setPassageId(res.data.test.passageId)
             })
             .catch((err) => console.error(err))
         }
@@ -301,3 +302,4 @@ function MatchingSentenceEndings() {
 }
 
 export default MatchingSentenceEndings;
+
