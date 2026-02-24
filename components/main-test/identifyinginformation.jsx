99ad0a2b968b62a1c5ac22c8f669574c4fa7cdfe
SolarCyclone <190097766+SolarCyclone@users.noreply.github.com>
2026-02-24 10:51:16 +0800

    Update identifyinginformation.jsx

--- components/main-test/identifyinginformation.jsx
+++ components/main-test/identifyinginformation.jsx
@@ -55,13 +55,14 @@ function IdentifyingInformation() {
         axios
             .get('/start-random-exam', {params: queryParams})
             .then((res) => {
+                console.log("Backend response:", res.data)
                 console.log("Number of question received", res.data.questions.length);
                 console.log("Questions Array:", res.data.questions);
                 // taking all questions from the randomizer (JSON)
-                setAllQuestions(res.data.questions);
+                setAllQuestions(res.data.test.questions);
                 // taking important details (JSON), set to passageHistory
-                setPassageHistory([res.data]);
-                setPassageId(res.data.passageId)
+                setPassageHistory([res.data.test]);
+                setPassageId(res.data.test.passageId)
             })
             .catch((err) => console.error(err));
         }
@@ -152,7 +153,7 @@ function IdentifyingInformation() {
         11: "Diagram Label Completion",
     };
 
-    if ((allQuestions || []).length === 0) return <h1>Loading...</h1>
+    if (!allQuestions || !passageHistory) return <h1>Loading...</h1>
 
     const sendUserAnswers = () => {
         
@@ -297,3 +298,4 @@ function IdentifyingInformation() {
 }
 
 export default IdentifyingInformation;
+
