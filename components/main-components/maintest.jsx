f25f8f187f931696a4f120a52a0a393cb7776116
SolarCyclone <190097766+SolarCyclone@users.noreply.github.com>
2026-02-26 18:12:41 +0800

    Update maintest.jsx

--- components/main-components/maintest.jsx
+++ components/main-components/maintest.jsx
@@ -103,7 +103,7 @@ function MTPage() {
         // Fetch the data for this specific test
         axios
             // change this post route if needed
-            .get('/test-selection', {params: queryParams, withCredentials: true})
+            .get('/test-selection', {params: queryParams}, {withCredentials: true})
             .then((res) => {
                 // Update all states with the response from the backend
                 //setCompleted(res.data.status);
